f95556ea1c3b35e897b630b91100135a512df882
Nate Jones <nate@endot.org>
2011-01-29 23:30:37 -0800

    svn vimdiff now shows descriptive names

--- bin/vimdiff.pl
+++ bin/vimdiff.pl
@@ -22,24 +22,54 @@ my $temp_file_re = qr{
 
 print STDERR join( ' ', @ARGV), "\n";
 
-my $file1 = $ARGV[5];
-my $file2 = $ARGV[6];
+# the left or previous file
+my $previous_file = $ARGV[5];
+
+# the right or new file
+my $new_file = $ARGV[6];
 
 # Some versions of subversion copy the modified file into tmp, thereby losing
 # any modifications you make in vimd will diffing.
 # Files get copied to tmp if they have svn:keywords.
 # http://stackoverflow.com/questions/396176/why-does-svn-diff-sometimes-copy-working-files-to-a-temp-file
 # newly created files will have both sides as a tmp file
-if ( $file1 =~ m{$temp_file_re} ) {
-	$file2 = $ARGV[2];
-	$file2 =~ s{ \t [(] .* \z }{}xsm;
+if ( $previous_file =~ m{$temp_file_re} ) {
+    $new_file = $ARGV[2];
+    $new_file =~ s{ \t [(] .* \z }{}xsm;
 }
-elsif ( $file2 =~ m{$temp_file_re} ) {
-    $file2 = $file1;
-    $file2 =~ s{ [.]svn/text-base/   }{}xsm;
-    $file2 =~ s{ [.]svn-base       \z}{}xsm;
+elsif ( $new_file =~ m{$temp_file_re} ) {
+    $new_file = $previous_file;
+    $new_file =~ s{ [.]svn/text-base/   }{}xsm;
+    $new_file =~ s{ [.]svn-base       \z}{}xsm;
 }
 
-my @cmd = ( "$diff_prog", "$file2", "$file1" );
+# get the description of the left/previous buffer and replace tab with space
+my $previous_file_desc = $ARGV[2];
+$previous_file_desc =~ s/\t/ /g;
+
+# same for the right/new buffer description
+my $new_file_desc = $ARGV[4];
+$new_file_desc =~ s/\t/ /g;
+
+# These commands make it so the names of the buffers in vimdiff make sense
+# first, we set the name we want in a buffer variable (b:name)
+# next, we update the statusline for the buffer (setlocal only modifies for
+#   current buffer) to be the same as the default status line with the exception
+#   that the file name is replaced by our description.
+# then, 'wincmd w' switches to the next (right) buffer, and we do the above for the other side.
+#
+# for reference, the default status line is: "%<%f\ %h%m%r%=%-14.(%l,%c%V%)\ %P"
+# type ":help statusline" in vim to see what that means
+# see also: https://wincent.com/wiki/Set_the_Vim_statusline
+
+my @VIM_COMMANDS = (
+    '-c', "let b:name=\"$previous_file_desc\"",
+    '-c', "setlocal statusline=%<%{b:name}\\ %h%m%r%=%-14.(%l,%c%V%)\\ %P",
+    '-c', "wincmd w",
+    '-c', "let b:name=\"$new_file_desc\"",
+    '-c', "setlocal statusline=%<%{b:name}\\ %h%m%r%=%-14.(%l,%c%V%)\\ %P",
+);
+
+my @cmd = ( "$diff_prog", @VIM_COMMANDS, "$previous_file", "$new_file" );
 print STDERR join( ' ', @cmd ), "\n";
 system( @cmd );
