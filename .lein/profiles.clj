e869c75ce2b8551024a14f4c15fba5987cf3da93
Nate Jones <nate@endot.org>
2015-01-20 16:59:18 -0800

    add lein-oneoff

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user  {:plugins [[cider/cider-nrepl  "0.6.1-SNAPSHOT"]
-                   [lein-autoreload    "0.1.0"]]}}
+                   [lein-autoreload    "0.1.0"]
+                   [lein-oneoff  "0.3.1"]]}}
