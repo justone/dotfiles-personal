30ea7928dbe1c8458029fe8665d5df8dbf163928
Nate Jones <nate@endot.org>
2018-08-14 13:12:02 -0700

    commented out scope capture

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user  {:plugins [[refactor-nrepl "2.3.1"]
                    [cider/cider-nrepl "0.16.0"]
                    [lein-oneoff  "0.3.1"]
-                   [lein-pprint  "1.1.1"]]}}
+                   [lein-pprint  "1.1.1"]]
+         ; :dependencies [[vvvvalvalval/scope-capture "0.1.4"]]
+         ; :injections [(require 'sc.api)]
+         }}
