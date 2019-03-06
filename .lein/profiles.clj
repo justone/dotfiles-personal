5167e14f347d745361c39269ec12c4abda0531d4
Nate Jones <nate@endot.org>
2019-03-06 08:32:28 -0800

    bump to 0.19.0 of cider, hide refactor-nrepl for now

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
-{:user  {:plugins [[refactor-nrepl "2.3.1"]
-                   [cider/cider-nrepl "0.16.0"]
+{:user  {:plugins [; [refactor-nrepl "2.3.1"]
+                   [cider/cider-nrepl "0.19.0"]
                    [lein-oneoff  "0.3.1"]
                    [lein-pprint  "1.1.1"]]
          ; :dependencies [[vvvvalvalval/scope-capture "0.1.4"]]
