e97e0a08fdf4add5b996104545bbc63ada3cd038
Nate Jones <nate@endot.org>
2019-04-19 14:30:51 -0700

    add useful lein aliases

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,4 +5,16 @@
          ; :dependencies [[vvvvalvalval/scope-capture "0.1.4"]]
          ; :injections [(require 'sc.api)]
          :repl-options {:timeout 120000}
-         }}
+         }
+
+ ;; kaocha test runner
+ :kaocha {:aliases
+          {"kaocha" ["run" "-m" "kaocha.runner"]}
+          :dependencies
+          [[lambdaisland/kaocha  "0.0-409"]]}
+
+ ;; allow specifying resources without leaving them modified
+ :dev-resources {:resource-paths ["scratch/dev-resources"]}
+
+ ;; allow specifying a bigger JVM
+ :hefty {:jvm-opts ^:replace ["-server" "-XX:MaxMetaspaceSize=1024m" "-Xmx3072m" "-XX:+UseG1GC"]}}
